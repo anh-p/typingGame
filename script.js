12bd0f0c2a172e2e9a623c5e68b0b6101be17bb8
Anh P <89045140+anh-p@users.noreply.github.com>
2022-10-17 21:14:37 -0400

    quotes rocket and confetti

--- script.js
+++ script.js
@@ -1,25 +1,68 @@
+const quotes = [
+    'Everything I know I learned from dogs',
+    'Dogs do speak, but only to those who know how to listen',
+    'Dogs lives are too short',
+    'Every dog must have his day',
+    'As wonderful as dogs can be, they are famous for missing the point',
+];
+
 const quote = document.getElementById('quote');
 const input = document.getElementById('typed-value');
 const start = document.getElementById('start');
+const message = document.getElementById('message');
 
-let targetWord;
+let wordQueue;
+let highlightPosition;
+let startTime;
 
-function startGame() {
-    console.log("Game started!");
 
-    targetWord = "typeme";
-    quote.innerHTML = `<span>${targetWord}</span>`;
+function startGame(){
+    console.log("Game started");
+    document.body.className = "";
+    start.className = "started";
+
+    const quoteIndex = Math.floor(Math.random() * quotes.length);
+    const quoteText = quotes[quoteIndex];
+
+    // quoteText = "type me";
+    wordQueue = quoteText.split(' ');
+    quote.innerHTML = wordQueue.map(word => (`<span>${word}</span>`)).join('');
+
+    highlightPosition = 0;
+    quote.childNodes[highlightPosition].className = 'highlight';
+
+    startTime = new Date().getTime();
 }
 start.addEventListener('click', startGame);
 input.addEventListener('input', checkInput);
 
 function checkInput() {
     console.log("Checking", input.value);
-    const currentWord = targetWord;
+    const currentWord = wordQueue[0].replaceAll(".", "").replaceAll(",", "");
     const typedValue = input.value.trim();
-    if (currentWord !== typedValue) {
+
+    if(currentWord !== typedValue){
         input.className = currentWord.startsWith(typedValue) ? "" : "error";
         return;
+    }
+    wordQueue.shift();
+    input.value = "";
+
+    quote.childNodes[highlightPosition].className = "";
+
+    if(wordQueue.length === 0){
+        gameOver();
+        return;
+    }
+    highlightPosition++;
+    quote.childNodes[highlightPosition].className = 'highlight';
 }
+
+function gameOver(){
+    const elapsedTime = new Date().getTime() - startTime;
+    document.body.className = "winner";
+    message.innerHTML = `<span class="congrats">Congratulations!</span>
+    <br>
+    You finished in ${elapsedTime/1000} seconds`;
 
 }
